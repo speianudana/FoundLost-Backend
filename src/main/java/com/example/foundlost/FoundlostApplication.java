e4d7de8f25ba7444abb2d9ce3605c5f22d7e18e6
Dana Speianu <speianu.dana@gmail.com>
2022-03-28 21:29:53 +0300

    project initiation

---
+++ src/main/java/com/example/foundlost/FoundlostApplication.java
@@ -0,0 +1,13 @@
+package com.example.foundlost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundlostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundlostApplication.class, args);
+	}
+
+}
