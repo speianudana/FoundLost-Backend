e779cac86cb5cd7866aedd55cb720cc864578053
Ion Dodon <iondodon2@gmail.com>
2022-05-31 18:43:44 +0300

    calculate distance

---
+++ src/main/java/com/pbl/foundlost/services/DistanceUnit.java
@@ -0,0 +1,5 @@
+package com.pbl.foundlost.services;
+
+public enum DistanceUnit {
+    KM, MILES, NAUTICAL_MILES
+}
--- src/main/java/com/pbl/foundlost/services/PostsService.java
+++ src/main/java/com/pbl/foundlost/services/PostsService.java
@@ -14,6 +14,9 @@ import java.math.MathContext;
 import java.util.List;
 import java.util.Objects;
 
+import static com.pbl.foundlost.services.DistanceUnit.KM;
+import static com.pbl.foundlost.services.DistanceUnit.NAUTICAL_MILES;
+import static java.math.BigDecimal.valueOf;
 import static java.util.Collections.emptyList;
 import static java.util.Objects.isNull;
 import static java.util.stream.Collectors.toList;
@@ -21,7 +24,7 @@ import static java.util.stream.Collectors.toList;
 @Service
 @RequiredArgsConstructor
 public class PostsService {
-    private final BigDecimal RANGE = new BigDecimal("0.002");
+    private final BigDecimal KM_RANGE = new BigDecimal("1");
 
     private final PostRepository postRepository;
 
@@ -45,7 +48,7 @@ public class PostsService {
     @Nullable
     private NearPostDto mapIfInRange(Post post, Post candidate) {
         BigDecimal distance = getDistance(post, candidate);
-        if (distance.compareTo(RANGE) <= 0) {
+        if (distance.compareTo(KM_RANGE) <= 0) {
             return new NearPostDto(candidate, distance);
         }
         return null;
@@ -54,10 +57,36 @@ public class PostsService {
     private BigDecimal getDistance(Post post, Post candidate) {
         MathContext mc = new MathContext(14);
         GeographicalLocation currentPostPosition = post.getGeographicalLocation();
-
         GeographicalLocation candidatePosition = candidate.getGeographicalLocation();
-        BigDecimal square1 = currentPostPosition.getLatitude().subtract(candidatePosition.getLatitude()).pow(2, mc);
-        BigDecimal square2 = currentPostPosition.getLongitude().subtract(candidatePosition.getLongitude()).pow(2, mc);
-        return square1.add(square2).sqrt(mc);
+        if (isNull(currentPostPosition) || isNull(candidatePosition)) {
+            return BigDecimal.ZERO;
+        }
+
+        double lat1 = currentPostPosition.getLatitude().doubleValue();
+        double lon1 = currentPostPosition.getLongitude().doubleValue();
+
+        double lat2 = candidatePosition.getLatitude().doubleValue();
+        double lon2 = candidatePosition.getLongitude().doubleValue();
+
+        return valueOf(getGeographicalDistance(lat1, lon1, lat2, lon2, KM));
+    }
+
+    public double getGeographicalDistance(double lat1, double lon1, double lat2, double lon2, DistanceUnit unit) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        } else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2))
+                    + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            if (KM.equals(unit)) {
+                dist = dist * 1.609344;
+            } else if (NAUTICAL_MILES.equals(unit)) {
+                dist = dist * 0.8684;
+            }
+            return (dist);
+        }
     }
 }
